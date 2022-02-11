957625037c0aa9b8f66407cfa4f54f7f3e299059
Benje Dev <Jasminlabidi@gmx.de>
2022-02-11 13:31:01 +0100

    insert footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import logo from "./Search_logo.png";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
- 
-      
       </header>
+      <footer className="footer">
+        <a
+          href="https://github.com/Benje-Dev/react-dictionary-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          Open-source code {""}
+        </a>
+        by Jasmin Labidi
+      </footer>
     </div>
   );
 }
-
-
